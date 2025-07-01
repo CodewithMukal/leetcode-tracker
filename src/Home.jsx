2d63e62c6e070a2b457dc5b964ffe6590518c3e5
CodewithMukal <mukal.github@gmail.com>
2025-07-01 13:00:35 +0530

    responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export const Home = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 w-fit mx-auto lg:gap-12 lg:my-6">
+      <div className="flex flex-col gap-4 justify-center items-center lg:grid lg:grid-cols-2 w-fit mx-auto lg:gap-12 lg:my-6">
         <Card user="mukalcode" />
         <Card user="thedevsumit" />
         <Card user="krishN_99" />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,8 @@ import "react-circular-progressbar/dist/styles.css";
 export const Card = (props) => {
   const [userInfo, setUser] = useState(null);
   const [hovered, setHover] = useState(false);
+  const [lastSubmission, setSub] = useState("");
+  const [daysAgo, setDaysAgo] = useState(0);
 
   const getResponse = async () => {
     const response = await fetch(
@@ -20,6 +22,23 @@ export const Card = (props) => {
       getResponse()
         .then((data) => {
           setUser(data);
+
+          const calendar = data.submissionCalendar;
+          const keys = Object.keys(calendar);
+
+          const utc = keys[keys.length - 1];
+
+          const latest = new Date(utc * 1000);
+          const lastSubmission = latest.toLocaleDateString();
+          const today = new Date();
+          const msPerDay = 1000 * 60 * 60 * 24;
+          const daysAgo = Math.floor(
+            (today.setHours(0, 0, 0, 0) - latest.setHours(0, 0, 0, 0)) /
+              msPerDay
+          );
+
+          setDaysAgo(daysAgo);
+          setSub(lastSubmission);
         })
         .catch((e) => {
           setUser({
@@ -36,24 +55,23 @@ export const Card = (props) => {
   //   console.log("userInfo:", userInfo ,userInfo.hardSolved,userInfo.totalHard);
 
   return (
-    <div className="scale-75 lg:scale-100">
-      <div className="text-white">
+      <div className="text-white text-xs lg:text-[16px]">
         {userInfo ? (
-          <div className="bg-[#505050]/36 rounded-[20px] ring-0 hover:ring-[1px] hover:ring-blue-400 w-200 flex ">
-            <div className="flex flex-col px-8 py-4 justify-center border-r-[1px] border-white items-center gap-4">
+          <div className="bg-[#505050]/30 ring-0 hover:ring-[1px] hover:ring-blue-400 flex ">
+            <div className="flex flex-col px-2 lg:px-8 lg:py-4 justify-center border-r-[1px] border-white items-center gap-4">
               <a
                 href={`https://leetcode.com/u/${props.user}`}
                 target="_blank"
                 className="flex justify-center items-center gap-2"
               >
-                <img className="w-10 h-10" src={logo} alt="" />
-                <h1 className="font-[Montserrat] font-bold text-xl">
+                <img className="lg:w-10 lg:h-10" src={logo} alt="" />
+                <h1 className="font-[Montserrat] font-bold lg:text-xl">
                   {props.user}
                 </h1>
               </a>
-              <h1 className="text-xl font-bold">Total Progress</h1>
+              <h1 className="lg:text-xl text-center font-bold">Total Progress</h1>
               <div
-                className="w-30 h-30 flex justify-center items-center"
+                className="w-15 h-15 lg:w-30 lg:h-30 flex justify-center items-center"
                 onMouseEnter={() => setHover(true)}
                 onMouseLeave={() => setHover(false)}
               >
@@ -70,7 +88,7 @@ export const Card = (props) => {
                     textSize: "16px",
                   })}
                 />
-                <div className="flex absolute font-bold justify-center items-center">
+                <div className="flex absolute text-xs lg:text-[16px] font-bold lg:scale-100 scale-65 justify-center items-center">
                   {hovered ? (
                     <div className="flex flex-col justify-center items-center">
                       <h1>{userInfo.acceptanceRate}%</h1>
@@ -94,17 +112,28 @@ export const Card = (props) => {
               <p className="font-semibold">
                 {userInfo.totalSolved}/{userInfo.totalQuestions}{" "}
               </p>
-              <p className="font-medium">
+              <p className="font-medium text-center">
                 Global Ranking: #{Number(userInfo.ranking).toLocaleString()}
               </p>
+              <div className="font-medium text-center">
+                Last Submission: {lastSubmission}
+                <br />
+                {daysAgo === 0 ? (
+                  <p>Today</p>
+                ) : daysAgo === 1 ? (
+                  <p>{daysAgo} day ago</p>
+                ) : (
+                  <p>{daysAgo} days ago</p>
+                )}
+              </div>
             </div>
             <div className="flex flex-col py-4">
-              <div className="border-b-[1px] my-4 gap-2 border-white flex flex-col justify-center items-center">
+              <div className="border-b-[1px] gap-2 lg:gap-2 border-white flex flex-col justify-center items-center">
                 <h1 className="font-bold">Total Solved Difficulty Wise:</h1>
-                <div className="flex h-fit gap-12">
-                  <div className="flex flex-col py-2 gap-4 justify-center items-center">
+                <div className="flex gap-3 lg:gap-12">
+                  <div className="flex flex-col py-2 lg:gap-4 justify-center items-center">
                     <h1 className="font-bold text-green-500">Easy:</h1>
-                    <div className="w-24 h-24 flex justify-center relative items-center">
+                    <div className="lg:w-24 lg:h-24 w-14 h-14 flex justify-center relative items-center">
                       <CircularProgressbar
                         value={(
                           (Number(userInfo.easySolved) /
@@ -131,9 +160,9 @@ export const Card = (props) => {
                       {userInfo.easySolved}/{userInfo.totalEasy}
                     </p>
                   </div>
-                  <div className="flex flex-col gap-4 justify-center items-center">
+                  <div className="flex flex-col lg:gap-4 justify-center items-center">
                     <h1 className="font-bold text-yellow-500">Medium:</h1>
-                    <div className="w-24 h-24 relative flex justify-center items-center">
+                    <div className="lg:w-24 lg:h-24 w-14 h-14 relative flex justify-center items-center">
                       <CircularProgressbar
                         value={(
                           (Number(userInfo.mediumSolved) /
@@ -160,9 +189,9 @@ export const Card = (props) => {
                       {userInfo.mediumSolved}/{userInfo.totalMedium}
                     </p>
                   </div>
-                  <div className="flex flex-col gap-4 justify-center items-center">
+                  <div className="flex flex-col lg:gap-4 justify-center items-center">
                     <h1 className="font-bold text-red-500">Hard:</h1>
-                    <div className="w-24 h-24 relative  flex justify-center items-center">
+                    <div className="lg:w-24 lg:h-24 w-14 h-14 relative  flex justify-center items-center">
                       <CircularProgressbar
                         value={(
                           (Number(userInfo.hardSolved) /
@@ -191,11 +220,11 @@ export const Card = (props) => {
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col font-bold py-3 px-2 gap-2">
+              <div className="flex flex-col text-center lg:text-left font-bold py-3 px-2 gap-2">
                 <h1>From total solved:</h1>
-                <div className="flex mx-4 gap-8 px-2">
-                  <div className="flex flex-col justify-center items-center">
-                    <div>
+                <div className="flex mx-4 lg:gap-8 px-2">
+                  <div className="flex flex-col gap-1 justify-center items-center">
+                    <div className="text-[10px] lg:text-[16px]">
                       Easy ={" "}
                       {(
                         (userInfo.easySolved / userInfo.totalSolved) *
@@ -203,12 +232,12 @@ export const Card = (props) => {
                       ).toFixed(2)}
                       %
                     </div>
-                    <div className="text-xs bg-white/45 py-1 px-2 rounded">
+                    <div className="text-[10px] lg:text-xs bg-white/45 py-1 px-2 rounded">
                       {userInfo.easySolved}/{userInfo.totalSolved}
                     </div>
                   </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <div>
+                  <div className="flex flex-col gap-1 justify-center items-center">
+                    <div className="text-[10px] lg:text-[16px]">
                       Medium ={" "}
                       {(
                         (userInfo.mediumSolved / userInfo.totalSolved) *
@@ -216,12 +245,12 @@ export const Card = (props) => {
                       ).toFixed(2)}
                       %
                     </div>
-                    <div className="text-xs bg-white/45 py-1 px-2 rounded">
+                    <div className="text-[10px] lg:text-xs bg-white/45 py-1 px-2 rounded">
                       {userInfo.mediumSolved}/{userInfo.totalSolved}
                     </div>
                   </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <div>
+                  <div className="flex flex-col gap-1 justify-center items-center">
+                    <div className="text-[10px] lg:text-[16px]">
                       Hard ={" "}
                       {(
                         (userInfo.hardSolved / userInfo.totalSolved) *
@@ -229,7 +258,7 @@ export const Card = (props) => {
                       ).toFixed(2)}
                       %
                     </div>
-                    <div className="text-xs bg-white/45 py-1 px-2 rounded">
+                    <div className="text-[10px] lg:text-xs bg-white/45 py-1 px-2 rounded">
                       {userInfo.hardSolved}/{userInfo.totalSolved}
                     </div>
                   </div>
@@ -244,6 +273,5 @@ export const Card = (props) => {
           </div>
         )}
       </div>
-    </div>
   );
 };
