557b3d8c141d9f52e47d77113bd41fd1041b3753
Mukal Markanda <mukal.github@gmail.com>
2025-07-04 23:35:51 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,11 +7,7 @@ export const Home = () => {
       <div className="flex flex-col gap-4 justify-center items-center lg:grid lg:grid-cols-2 w-fit mx-auto lg:gap-12 lg:my-6">
         <Card user="mukalcode" />
         <Card user="thedevsumit" />
-        <div>
-          <h1 className="text-3xl font-bold text-white">
-            Krish Negi will not be remembered
-          </h1>
-        </div>
+         <Card user="krishN_99" />
         <Card user="vedant_8075" />
         <Card user="aravtyagi0001" />
         <Card user="Ankush_Kharb" />
